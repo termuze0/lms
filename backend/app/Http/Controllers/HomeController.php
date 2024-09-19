ff0af5a11f3c7e11d44299ec6481350d3dbbe2a5
termuze <wertman99999@gmail.com>
2024-09-18 23:40:39 -0400

    homecontroller implemented

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        // Fetch latest courses and instructors
+        $latestCourses = Course::latest()->take(5)->get();
+        $instructors = Instructor::all();
+
+        return view('home', compact('latestCourses', 'instructors'));
+}
 }
