c290cc01016e0c4ca7eff975b4ddd4d405a4d7ff
termuze <wertman99999@gmail.com>
2024-09-23 20:52:17 -0400

    lesson model and migration

---
+++ backend/database/migrations/2024_09_24_005110_create_lessons_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLessonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id'); // Foreign key to Course
+            $table->string('title');
+            $table->text('content')->nullable();
+            $table->string('video_url')->nullable();
+            $table->timestamps();
+            
+            // Foreign key constraint
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lessons');
+    }
+}
